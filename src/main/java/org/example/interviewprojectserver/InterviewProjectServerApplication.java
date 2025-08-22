04401c2027ed69ff720131d9dbdf674e2ddefb43
AO-5002 <ao2005ortiz@gmail.com>
2025-08-22 18:23:23 -0500

    git init

---
+++ src/main/java/org/example/interviewprojectserver/InterviewProjectServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.interviewprojectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewProjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewProjectServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/interviewprojectserver/InterviewProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.interviewprojectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewProjectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
